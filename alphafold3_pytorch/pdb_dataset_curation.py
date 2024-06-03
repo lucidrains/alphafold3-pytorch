a1d821d9bfc96231311c8a31af7d5402cf2dd74d
Alex Morehead <alex.morehead@gmail.com>
2024-06-03 15:45:38 -0500

    Simplify timeout decorator in pdb_dataset_curation.py

--- alphafold3_pytorch/pdb_dataset_curation.py
+++ alphafold3_pytorch/pdb_dataset_curation.py
@@ -689,43 +689,6 @@ def write_structure(structure: Structure, output_filepath: str):
     io.save(output_filepath)
 
 
-@timeout_decorator.timeout(PROCESS_STRUCTURE_MAX_SECONDS, use_signals=False)
-def _process_structure(filepath: str, output_dir: str, skip_existing: bool = False):
-    """
-    Given an input mmCIF file, create a new processed mmCIF file
-    using AlphaFold 3's PDB dataset filtering criteria under a
-    timeout condition.
-    """
-    # Section 2.5.4 of the AlphaFold 3 supplement
-    structure = parse_structure(filepath)
-    output_file_dir = os.path.join(output_dir, structure.id[1:3])
-    output_filepath = os.path.join(output_file_dir, f"{structure.id}.cif")
-    if skip_existing and os.path.exists(output_filepath):
-        print(f"Skipping existing output file: {output_filepath}")
-        return
-    os.makedirs(output_file_dir, exist_ok=True)
-
-    # Filtering of targets
-    structure = filter_target(structure)
-    if exists(structure):
-        # Filtering of bioassemblies
-        structure = remove_hydrogens(structure)
-        structure = remove_all_unknown_residue_chains(structure, STANDARD_RESIDUES)
-        structure = remove_clashing_chains(structure)
-        structure = remove_excluded_ligands(structure, LIGAND_EXCLUSION_LIST)
-        structure = remove_non_ccd_atoms(structure, CCD_READER_RESULTS)
-        structure = remove_leaving_atoms(structure, CCD_READER_RESULTS)
-        structure = filter_large_ca_distances(structure)
-        structure = select_closest_chains(
-            structure, PROTEIN_RESIDUE_CENTER_ATOMS, NUCLEIC_ACID_RESIDUE_CENTER_ATOMS
-        )
-        structure = remove_crystallization_aids(structure, CRYSTALLOGRAPHY_METHODS)
-        if list(structure.get_chains()):
-            # Save processed structure
-            write_structure(structure, output_filepath)
-            print(f"Finished processing structure: {structure.id}")
-
-
 def process_structure(args: Tuple[str, str, bool]):
     """
     Given an input mmCIF file, create a new processed mmCIF file
@@ -733,7 +696,35 @@ def process_structure(args: Tuple[str, str, bool]):
     """
     filepath, output_dir, skip_existing = args
     try:
-        _process_structure(filepath, output_dir, skip_existing)
+        with timeout_decorator.timeout(PROCESS_STRUCTURE_MAX_SECONDS, use_signals=False):
+            # Section 2.5.4 of the AlphaFold 3 supplement
+            structure = parse_structure(filepath)
+            output_file_dir = os.path.join(output_dir, structure.id[1:3])
+            output_filepath = os.path.join(output_file_dir, f"{structure.id}.cif")
+            if skip_existing and os.path.exists(output_filepath):
+                print(f"Skipping existing output file: {output_filepath}")
+                return
+            os.makedirs(output_file_dir, exist_ok=True)
+
+            # Filtering of targets
+            structure = filter_target(structure)
+            if exists(structure):
+                # Filtering of bioassemblies
+                structure = remove_hydrogens(structure)
+                structure = remove_all_unknown_residue_chains(structure, STANDARD_RESIDUES)
+                structure = remove_clashing_chains(structure)
+                structure = remove_excluded_ligands(structure, LIGAND_EXCLUSION_LIST)
+                structure = remove_non_ccd_atoms(structure, CCD_READER_RESULTS)
+                structure = remove_leaving_atoms(structure, CCD_READER_RESULTS)
+                structure = filter_large_ca_distances(structure)
+                structure = select_closest_chains(
+                    structure, PROTEIN_RESIDUE_CENTER_ATOMS, NUCLEIC_ACID_RESIDUE_CENTER_ATOMS
+                )
+                structure = remove_crystallization_aids(structure, CRYSTALLOGRAPHY_METHODS)
+                if list(structure.get_chains()):
+                    # Save processed structure
+                    write_structure(structure, output_filepath)
+                    print(f"Finished processing structure: {structure.id}")
     except Exception as e:
         print(f"Skipping structure processing of {filepath} due to: {e}")
         structure_id = os.path.splitext(os.path.basename(filepath))[0]
