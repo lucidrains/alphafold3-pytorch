9ea3df4950a28c488b204342c5506c039ddcd776
lucidrains <lucidrains@gmail.com>
2024-06-08 08:21:02 -0700

    quick fix

--- alphafold3_pytorch/inputs.py
+++ alphafold3_pytorch/inputs.py
@@ -11,12 +11,12 @@ from alphafold3_pytorch.typing import (
 class AtomInput(TypedDict):
     atom_inputs:                Float['*b m dai']
     molecule_atom_lens:         Int['*b n']
-    atompair_inputs:            Float['*b m m dapi'] | Float['nw w (w*2) dapi']
+    atompair_inputs:            Float['*b m m dapi'] | Float['*b nw w (w*2) dapi']
     additional_molecule_feats:  Float['*b n 10']
     templates:                  Float['*b t n n dt']
     msa:                        Float['*b s n dm']
     atom_ids:                   Int['*b m'] | None
-    atompair_ids:               Int['*b m m'] | Int['nw w (w*2)'] | None
+    atompair_ids:               Int['*b m m'] | Int['*b nw w (w*2)'] | None
     template_mask:              Bool['*b t'] | None
     msa_mask:                   Bool['*b s'] | None
     atom_pos:                   Float['*b m 3'] | None
