516420f1f8922dca2cadf08b7acde9a879de390a
Phil Wang <lucidrains@gmail.com>
2024-06-29 14:58:53 -0700

    use `molecule_token_pool_lens` that was passed in for total number of atoms

--- alphafold3_pytorch/inputs.py
+++ alphafold3_pytorch/inputs.py
@@ -153,22 +153,17 @@ def molecule_to_atom_input(
 
     molecules = mol_input.molecules
 
+    # get total number of atoms
+
+    atom_lens = torch.tensor(mol_input.molecule_token_pool_lens)
+    total_atoms = atom_lens.sum().item()
+
     # molecule_atom_lens
 
     atoms = []
-    atom_lens = []
 
-    for mol, is_ligand in zip(molecules, mol_input.is_molecule_types[:, -1]):
-        num_atoms = mol.GetNumAtoms()
-
-        if is_ligand:
-            atom_lens.extend([1] * num_atoms)
-        else:
-            atom_lens.append(num_atoms)
-
+    for mol in molecules:
         atoms.extend([*mol.GetAtoms()])
-
-    total_atoms = sum(atom_lens)
 
     # handle maybe atom embeds
 
