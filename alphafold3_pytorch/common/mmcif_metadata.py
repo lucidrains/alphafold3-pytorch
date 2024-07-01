bff0cd08e37f153015242d4b180f62da3d40c35e
Phil Wang <lucidrains@gmail.com>
2024-07-01 10:00:00 -0700

    fetch versions from importlib.metadata

--- alphafold3_pytorch/common/mmcif_metadata.py
+++ alphafold3_pytorch/common/mmcif_metadata.py
@@ -4,7 +4,9 @@ from typing import Mapping, Sequence
 
 import numpy as np
 
-alphafold_version = "3.0.0"
+from importlib.metadata import version
+
+alphafold_version = version("alphafold3_pytorch")
 
 _DISCLAIMER = """THE INFORMATION IS NOT INTENDED FOR, HAS NOT BEEN VALIDATED FOR, AND IS NOT
 APPROVED FOR CLINICAL USE. IT SHOULD NOT BE USED FOR CLINICAL PURPOSE OR RELIED
