9524ba935aeeac09f184f21e0a6ee398d99c4479
Heng Cai <abtion@outlook.com>
2024-07-04 20:10:44 +0800

    [update] the alignment error should be a pairwise error.

--- alphafold3_pytorch/alphafold3.py
+++ alphafold3_pytorch/alphafold3.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from functools import partial, wraps
 from collections import namedtuple
 
+import einops
 import torch
 from torch import nn
 from torch import Tensor
@@ -2530,23 +2531,35 @@ class ComputeAlignmentError(Module):
         true_coords: Float['b n 3'],
         pred_frames: Float['b n 3 3'],
         true_frames: Float['b n 3 3']
-    ) -> Float['b n']:
+    ) -> Float['b n n']:
         """
         pred_coords: predicted coordinates
         true_coords: true coordinates
         pred_frames: predicted frames
         true_frames: true frames
         """
+        num_res = pred_coords.shape[1]
+        
+        pair2seq = partial(einops.rearrange, pattern='b n m ... -> b (n m) ...')
+        seq2pair = partial(einops.rearrange, pattern='b (n m) ... -> b n m ...', n = num_res, m = num_res)
+        
+        pair_pred_coords = pair2seq(einops.repeat(pred_coords, 'b n d -> b n m d', m = num_res))
+        pair_true_coords = pair2seq(einops.repeat(true_coords, 'b n d -> b n m d', m = num_res))
+        pair_pred_frames = pair2seq(einops.repeat(pred_frames, 'b n d e -> b m n d e', m = num_res))
+        pair_true_frames = pair2seq(einops.repeat(true_frames, 'b n d e -> b m n d e', m = num_res))
+        
         # Express predicted coordinates in predicted frames
-        pred_coords_transformed = self.express_coordinates_in_frame(pred_coords, pred_frames)
+        pred_coords_transformed = self.express_coordinates_in_frame(pair_pred_coords, pair_pred_frames)
 
         # Express true coordinates in true frames
-        true_coords_transformed = self.express_coordinates_in_frame(true_coords, true_frames)
+        true_coords_transformed = self.express_coordinates_in_frame(pair_true_coords, pair_true_frames)
 
         # Compute alignment errors
         alignment_errors = torch.sqrt(
             torch.sum((pred_coords_transformed - true_coords_transformed) ** 2, dim=-1) + self.eps
         )
+        
+        alignment_errors = seq2pair(alignment_errors)
 
         return alignment_errors
 
