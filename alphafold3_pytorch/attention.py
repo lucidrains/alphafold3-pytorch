3a85f388cdbe882fb502a26de62852bcf4978785
lucidrains <lucidrains@gmail.com>
2024-07-04 07:35:53 -0700

    address https://github.com/lucidrains/alphafold3-pytorch/issues/57

--- alphafold3_pytorch/attention.py
+++ alphafold3_pytorch/attention.py
@@ -18,13 +18,6 @@ from alphafold3_pytorch.tensor_typing import (
     typecheck
 )
 
-# constants
-
-class Config(NamedTuple):
-    enable_flash: bool
-    enable_math: bool
-    enable_mem_efficient: bool
-
 # helpers
 
 def exists(val):
@@ -167,10 +160,8 @@ class Attention(Module):
         dropout = 0.,
         gate_output = True,
         query_bias = True,
-        flash = False,
         window_size = None,
         num_memory_kv: int = 0,
-        efficient_attn_config: Config = Config(True, True, True),
         enable_attn_softclamp = False,
         attn_softclamp_value = 30.
     ):
@@ -191,10 +182,8 @@ class Attention(Module):
         dim_inner = dim_head * heads
 
         self.attend = Attend(
-            flash = flash,
             dropout = dropout,
             window_size = window_size,
-            attn_config = efficient_attn_config,
             enable_attn_softclamp = enable_attn_softclamp,
             attn_softclamp_value = attn_softclamp_value,
         )
@@ -266,16 +255,14 @@ class Attention(Module):
 
         return self.to_out(out)
 
-# attending, both vanilla as well as in-built flash attention
+# the main attention function
 
 class Attend(Module):
     def __init__(
         self,
         dropout = 0.,
-        flash = False,
         window_size = None,
         scale: float | None = None,
-        attn_config: Config = Config(True, True, True),
         enable_attn_softclamp = False,
         attn_softclamp_value = 30.
     ):
@@ -299,43 +286,13 @@ class Attend(Module):
         self.is_local_attn = exists(window_size)
         self.window_size = window_size
 
-        self.flash = flash
-        self.attn_config = attn_config
         self.attn_dropout = nn.Dropout(dropout)
-
-        assert not (flash and enable_attn_softclamp), 'softclamp of attn logits not compatible with flash yet'
 
         # softclamp attention logits
         # being adopted by a number of recent llms (gemma, grok)
 
         self.enable_attn_softclamp = enable_attn_softclamp
         self.attn_softclamp_value = attn_softclamp_value
-
-    @typecheck
-    def flash_attn(
-        self,
-        q: Float['b h i d'],
-        k: Float['b h j d'],
-        v: Float['b h j d'],
-        mask: Bool['b j'] | None = None
-    ) -> Float['b h i d']:
-
-        _, heads, seq_len, _ = q.shape
-
-        attn_mask = None
-
-        if exists(mask):
-            mask = repeat(mask, 'b j -> b h i j', h = heads, i = seq_len)
-
-        with torch.backends.cuda.sdp_kernel(**self.attn_config._asdict()):
-            out = F.scaled_dot_product_attention(
-                q, k, v,
-                attn_mask = attn_mask,
-                scale = self.scale,
-                dropout_p = self.dropout if self.training else 0.
-            )
-
-        return out
 
     @typecheck
     def local_attn(
@@ -502,13 +459,6 @@ class Attend(Module):
 
             if exists(mask):
                 mask = pad_at_dim(mask, (num_mem_kv, 0), value = True)
-
-        # forward to using flash attention if applicable
-
-        can_use_flash = self.flash and not exists(attn_bias), 'flash attention does not support attention bias with gradients'
-
-        if can_use_flash:
-            return self.flash_attn(q, k, v, mask = mask)
 
         # default attention
 
