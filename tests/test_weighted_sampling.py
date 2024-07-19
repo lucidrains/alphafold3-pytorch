cbaf109e743f90d84520110d52cbd55971132b7d
Alex Morehead <alex.morehead@gmail.com>
2024-07-19 18:56:01 -0500

    Update test_weighted_sampling.py

--- tests/test_weighted_sampling.py
+++ tests/test_weighted_sampling.py
@@ -1,20 +1,36 @@
+import os
+
 import pytest
-from alphafold3_pytorch.data.weighted_pdb_dataset import WeightedSamplerPDB
+
+from torch.utils.data import Sampler
+
+from alphafold3_pytorch.data.weighted_pdb_sampler import WeightedPDBSampler
+
 
 @pytest.fixture
-def dataset():
-    interface_mapping_path = "data/test/interface_cluster_mapping.csv"
+def sampler():
+    """Return a `WeightedPDBSampler` object."""
+    interface_mapping_path = os.path.join("data", "test", "interface_cluster_mapping.csv")
     chain_mapping_paths = [
-        "data/test/ligand_chain_cluster_mapping.csv",
-        "data/test/nucleic_acid_chain_cluster_mapping.csv",
-        "data/test/peptide_chain_cluster_mapping.csv",
-        "data/test/protein_chain_cluster_mapping.csv",
+        os.path.join("data", "test", "ligand_chain_cluster_mapping.csv"),
+        os.path.join("data", "test", "nucleic_acid_chain_cluster_mapping.csv"),
+        os.path.join("data", "test", "peptide_chain_cluster_mapping.csv"),
+        os.path.join("data", "test", "protein_chain_cluster_mapping.csv"),
     ]
-    return WeightedSamplerPDB(
+    return WeightedPDBSampler(
         chain_mapping_paths=chain_mapping_paths,
         interface_mapping_path=interface_mapping_path,
         batch_size=4,
     )
 
-def test_sample(dataset):
-    assert len(dataset.sample(4)) == 4, "The sampled batch size does not match the expected size."
+
+def test_sample(sampler: Sampler):
+    """Test the sampling method of the `WeightedSamplerPDB` class."""
+    assert len(sampler.sample(4)) == 4, "The sampled batch size does not match the expected size."
+
+
+def test_cluster_based_sample(sampler: Sampler):
+    """Test the cluster-based sampling method of the `WeightedSamplerPDB` class."""
+    assert (
+        len(sampler.cluster_based_sample(4)) == 4
+    ), "The cluster-based sampled batch size does not match the expected size."
