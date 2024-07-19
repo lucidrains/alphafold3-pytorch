47fddb6c6e7cfcc4134e304940d812ecd90bd8e3
vandrw <v.andrei1455@gmail.com>
2024-07-19 20:09:50 +0200

    Add example `*_cluster_mapping.csv` files for testing

--- tests/test_weighted_sampling.py
+++ tests/test_weighted_sampling.py
@@ -3,18 +3,18 @@ from alphafold3_pytorch.data.weighted_pdb_dataset import WeightedSamplerPDB
 
 @pytest.fixture
 def dataset():
-    interface_mapping_path = "data/pdb_data/data_caches/clusterings/interface_cluster_mapping.csv"
+    interface_mapping_path = "data/test/interface_cluster_mapping.csv"
     chain_mapping_paths = [
-        "data/pdb_data/data_caches/clusterings/ligand_chain_cluster_mapping.csv",
-        "data/pdb_data/data_caches/clusterings/nucleic_acid_chain_cluster_mapping.csv",
-        "data/pdb_data/data_caches/clusterings/peptide_chain_cluster_mapping.csv",
-        "data/pdb_data/data_caches/clusterings/protein_chain_cluster_mapping.csv",
+        "data/test/ligand_chain_cluster_mapping.csv",
+        "data/test/nucleic_acid_chain_cluster_mapping.csv",
+        "data/test/peptide_chain_cluster_mapping.csv",
+        "data/test/protein_chain_cluster_mapping.csv",
     ]
     return WeightedSamplerPDB(
         chain_mapping_paths=chain_mapping_paths,
         interface_mapping_path=interface_mapping_path,
-        batch_size=64,
+        batch_size=4,
     )
 
 def test_sample(dataset):
-    assert len(dataset.sample(64)) == 64, "The sampled batch size does not match the expected size."
+    assert len(dataset.sample(4)) == 4, "The sampled batch size does not match the expected size."
