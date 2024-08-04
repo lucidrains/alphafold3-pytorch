7f75ff0153eefc0eb468499762a4c50be2ad2904
lucidrains <lucidrains@gmail.com>
2024-08-04 13:58:11 -0700

    one more time

--- tests/test_input.py
+++ tests/test_input.py
@@ -95,6 +95,7 @@ def test_alphafold3_input(directed_bonds):
         atoms_per_window = 27,
         dim_template_feats = 44,
         num_dist_bins = 38,
+        num_molecule_mods = 0,
         confidence_head_kwargs = dict(
             pairformer_depth = 1
         ),
@@ -147,6 +148,7 @@ def test_atompos_input():
         atoms_per_window = 27,
         dim_template_feats = 44,
         num_dist_bins = 38,
+        num_molecule_mods = 0,
         confidence_head_kwargs = dict(
             pairformer_depth = 1
         ),
