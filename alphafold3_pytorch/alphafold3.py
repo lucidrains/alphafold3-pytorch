a53d0ba8913b0cd26f89855b72ed4b31357d4bc4
Alex Morehead <alex.morehead@gmail.com>
2024-08-06 12:58:59 -0500

    Add proper batching support for masking out missing sampled atom positions (#154)
    * Update alphafold3.py
    
    * Update test_input.py
    
    * Update alphafold3.py
    
    * Update test_input.py

--- alphafold3_pytorch/alphafold3.py
+++ alphafold3_pytorch/alphafold3.py
@@ -96,7 +96,6 @@ h - heads
 n - molecule sequence length
 i - molecule sequence length (source)
 j - molecule sequence length (target)
-l - present (i.e., non-missing) atom sequence length
 m - atom sequence length
 nw - windowed sequence length
 d - feature dimension
@@ -4834,14 +4833,12 @@ class Alphafold3(Module):
         resolved_labels: Int['b n'] | Int['b m'] | None = None,
         return_loss_breakdown = False,
         return_loss: bool = None,
-        return_present_sampled_atoms: bool = False,
         return_confidence_head_logits: bool = False,
         num_rollout_steps: int | None = None,
         rollout_show_tqdm_pbar: bool = False,
         detach_when_recycling: bool = None
     ) -> (
         Float['b m 3'] |
-        Float['l 3'] |
         Tuple[Float['b m 3'] | Float['l 3'], ConfidenceHeadLogits] |
         Float[''] |
         Tuple[Float[''], LossBreakdown]
@@ -5103,9 +5100,6 @@ class Alphafold3(Module):
 
             if return_confidence_head_logits:
                 confidence_head_atom_pos_input = sampled_atom_pos.clone()
-
-            if exists(missing_atom_mask) and return_present_sampled_atoms:
-                sampled_atom_pos = sampled_atom_pos[~missing_atom_mask]
 
             if not return_confidence_head_logits:
                 return sampled_atom_pos
