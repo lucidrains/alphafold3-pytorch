d9270d31ec6b296005a06e28b326bba2e67506ec
Alex Morehead <alex.morehead@gmail.com>
2024-08-21 17:55:09 -0500

    Update amino_acid_constants.py

--- alphafold3_pytorch/common/amino_acid_constants.py
+++ alphafold3_pytorch/common/amino_acid_constants.py
@@ -117,6 +117,35 @@ restype_1to3 = {
     "X": "UNK",
 }
 
+MSA_CHAR_TO_ID = {
+    "A": 0,
+    "R": 1,
+    "N": 2,
+    "D": 3,
+    "C": 4,
+    "Q": 5,
+    "E": 6,
+    "G": 7,
+    "H": 8,
+    "I": 9,
+    "L": 10,
+    "K": 11,
+    "M": 12,
+    "F": 13,
+    "P": 14,
+    "S": 15,
+    "T": 16,
+    "W": 17,
+    "Y": 18,
+    "V": 19,
+    "X": 20,
+    "-": 31,
+    "U": 1,
+    "Z": 3,
+    "J": 20,
+    "O": 20,
+}
+
 BIOMOLECULE_CHAIN: Final[str] = "polypeptide(L)"
 POLYMER_CHAIN: Final[str] = "polymer"
 
