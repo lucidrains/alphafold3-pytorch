b134f0e8985e37216a43c5d1a27059595f8c4018
Alex Morehead <alex.morehead@gmail.com>
2024-09-14 20:58:30 -0500

    Update inputs.py

--- alphafold3_pytorch/inputs.py
+++ alphafold3_pytorch/inputs.py
@@ -2847,35 +2847,30 @@ def pdb_input_to_molecule_input(
         # NOTE: this is the template cutoff date for all inference tasks
         template_cutoff_date = datetime.strptime("2021-09-30", "%Y-%m-%d")
 
-    templates_dir = i.templates_dir
-    num_templates_per_chain = i.num_templates_per_chain
-
     if (
         exists(i.max_num_template_tokens)
         and num_tokens * i.num_templates_per_chain > i.max_num_template_tokens
     ):
-        templates_dir = None
-        num_templates_per_chain = 1
-
         if verbose:
             logger.warning(
                 f"The number of tokens ({num_tokens}) multiplied by the number of templates per structure ({i.num_templates_per_chain}) exceeds the maximum total number of template tokens {(i.max_num_template_tokens)}. "
-                "Skipping curation of templates features for this example by installing a dummy template for each chain."
+                "Skipping curation of templates features for this example."
             )
-
-    template_features = load_templates_from_templates_dir(
-        # NOTE: if templates are not locally available, no template features will be used
-        templates_dir,
-        mmcif_dir,
-        file_id,
-        chain_id_to_residue,
-        max_templates_per_chain=i.max_templates_per_chain,
-        num_templates_per_chain=num_templates_per_chain,
-        kalign_binary_path=i.kalign_binary_path,
-        template_cutoff_date=template_cutoff_date,
-        randomly_sample_num_templates=exists(i.training) and i.training,
-        verbose=verbose,
-    )
+        template_features = {}
+    else:
+        template_features = load_templates_from_templates_dir(
+            # NOTE: if templates are not locally available, no template features will be used
+            i.templates_dir,
+            mmcif_dir,
+            file_id,
+            chain_id_to_residue,
+            max_templates_per_chain=i.max_templates_per_chain,
+            num_templates_per_chain=i.num_templates_per_chain,
+            kalign_binary_path=i.kalign_binary_path,
+            template_cutoff_date=template_cutoff_date,
+            randomly_sample_num_templates=exists(i.training) and i.training,
+            verbose=verbose,
+        )
 
     templates = template_features.get("templates")
     template_mask = template_features.get("template_mask")
