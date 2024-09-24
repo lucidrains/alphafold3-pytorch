dee1e97c6db5ffb746cca02ca4813af5657f240b
Phil Wang <lucidrains@gmail.com>
2024-09-24 11:17:47 -0700

    use choosenim from official nim-lang

--- install-nim.sh
+++ install-nim.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-os_type=$(uname)
-
-if [[ "$os_type" == "Darwin" ]]; then
-  brew install nim
-elif [[ "$os_type" == "Linux" ]]; then
-  apt-get install nim
-else
-  echo "This is not a Mac or Linux system."
-  exit
-fi
-
+curl https://nim-lang.org/choosenim/init.sh -sSf | sh
+choosenim 2.0.8
 nimble install nimpy
